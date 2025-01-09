595cb5c57200db821b955d7bd1485f06304f75b6
gabriel <gabrielpr9520@gmail.com>
2025-01-09 12:30:20 +0000

    subido al repositorio

---
+++ database/migrations/2025_01_09_100146_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('genre');
+            $table->double('price');
+            $table->string('developer');
+            $table->integer('year');
+            $table->string('cover');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
